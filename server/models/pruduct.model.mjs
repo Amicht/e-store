801628d47d48e44fdb2898b37f3994678e3015ad
Amicht <vicolicht@gmail.com>
2022-08-03 20:14:31 +0300

    models added

--- server/models/pruduct.model.mjs
+++ server/models/pruduct.model.mjs
@@ -4,6 +4,13 @@ class ProductModel{
     category_id; 
     price; 
     image;
+    constructor(product){
+        this.id = null;
+        this.name= product.name;
+        this.category_id= +product.category_id;
+        this.price= +product.price;
+        this.image= product.image;
+    }
 }
 
-export default ProductModel;
+export { ProductModel };
