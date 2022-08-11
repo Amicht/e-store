3bd038f67b15e2207bfa5baa5ad9ca00e7166b5d
Amicht <vicolicht@gmail.com>
2022-08-11 18:17:32 +0300

    admin main page fixed

--- client/src/app/pages/admin/admin.component.ts
+++ client/src/app/pages/admin/admin.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Category } from 'src/app/models/category.model';
-import { ProductRes } from 'src/app/models/product.model';
+import { ProductReq, ProductRes } from 'src/app/models/product.model';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -10,20 +10,25 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class AdminComponent implements OnInit {
 
-  adminScreen: "add" | "update" = "add";
+  adminScreen: string = "add";
   products:ProductRes[] = [];
   categories: Category[] = [];
-  productToEdit: ProductRes | undefined = undefined;
+  message: string = "";
+  productToEdit: ProductRes = {category_id:0,id:0,image:"",name:"",price:0};
+
   constructor(private productService:ProductService) { }
+
   ngOnInit(): void {
     this.productService.products$.subscribe(p => this.products = p);
     this.productService.loadProducts();
     this.getCategories();
   }
-  changeAdminScreen(product:ProductRes){
-    console.log(product);
-    this.productToEdit = product;
-    this.adminScreen = "update";
+  changeAdminScreen(event:{product:ProductRes, action:string}){
+    this.productToEdit = event.product;
+    this.setScreen(event.action);
+  }
+  setScreen(adminScreen:string){
+    this.adminScreen = adminScreen;
   }
   setCategory(category:string){
     if(category === "*"){
@@ -36,5 +41,21 @@ export class AdminComponent implements OnInit {
   async getCategories(){
     this.categories = await this.productService.loadFoodCategories()
   }
-
+  updateProduct(product:ProductRes){
+    this.productService.updateProduct(product)
+    .then(message => this.setMessageScreen(message))
+  }
+  deleteProduct(id:number){
+    this.productService.deleteProduct(id)
+    .then(message => this.setMessageScreen(message))
+  }
+  setMessageScreen(message:string){
+    this.message = message;
+    this.setScreen("message");
+    this.productService.loadProducts();
+  }
+  addProduct(newProduct:FormData){
+    this.productService.addProduct(newProduct)
+    .then(message => this.setMessageScreen(message))
+  }
 }
