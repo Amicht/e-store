27eaea52772c82a07a8dd30438f82645d2fbd730
Amicht <vicolicht@gmail.com>
2022-08-11 18:16:36 +0300

    update merhod fixed

--- client/src/app/pages/admin/update-product/update-product.component.ts
+++ client/src/app/pages/admin/update-product/update-product.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ProductRes } from 'src/app/models/product.model';
 
@@ -7,17 +7,27 @@ import { ProductRes } from 'src/app/models/product.model';
   templateUrl: './update-product.component.html',
   styleUrls: ['./update-product.component.scss']
 })
-export class UpdateProductComponent implements OnInit {
+export class UpdateProductComponent implements OnInit ,OnChanges{
 
-  @Input() product!:ProductRes | undefined;
+  @Input() product!:ProductRes;
+  @Output() editHandler = new EventEmitter()
   updateForm!:FormGroup;
   constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.setForm()
+  }
+  ngOnChanges(): void {
+    this.setForm()
+  }
+  setForm(){
     this.updateForm = this.fb.group({
-      name:[this.product!.name,[Validators.required]],
-      price:[this.product!.price,[Validators.required]]
+      name:[this.product.name,[Validators.required]],
+      price:[this.product.price,[Validators.required, Validators.min(0.5)]]
     })
+  }
+  onSubmit(){
+    this.editHandler.emit({...this.product, ...this.updateForm.value})
   }
 
 }
